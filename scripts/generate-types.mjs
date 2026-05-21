a64cddd402d3c0bcdf509d58d16719bb0f379b61
Nisarg <solankinisarg815@gmail.com>
2026-05-21 08:14:34 +0530

    Add the OPENAPI_DOCS_URL for the vercel

--- scripts/generate-types.mjs
+++ scripts/generate-types.mjs
@@ -1,8 +1,7 @@
 import { execSync } from "child_process";
 
 const url =
-  process.env.NEXT_PUBLIC_API_URL ||
-  "http://localhost:4000/api/v1/api-docs.json";
+  process.env.OPENAPI_DOCS_URL || "http://localhost:4000/api/v1/api-docs.json";
 
 execSync(`openapi-typescript ${url} -o src/types/api.d.ts`, {
   stdio: "inherit",
